b7bc9585ad0d211c4f682f411c0246c3f5a34725
josepaez2 <josepaez_2@hotmail.com>
2017-06-29 17:00:31 -0500

    version1

--- solution.rb
+++ solution.rb
@@ -1,37 +1,11 @@
 require "sinatra"
-#Lee .txts
-def page_content(titulo)
-  File.read("pages/#{titulo}.txt")
-rescue Errno::ENOENT
-  return nil
-end
-#Crea .txts
-def save_content(titulo, contenido)
-  File.open("pages/#{titulo}.txt", "w") do |file|
-    file.print(contenido)
-  end
-end
-#Destruye txts
-def delete_content(titulo, contenido)
-  File.delete("pages/#{titulo}.txt")
-end
-#Página de Welcome
-get "/" do
-   erb :bienvenido
-end
-
-#Receptor de FORMS
-post "/crear" do
-save_content(params["titulo"], params["contenido"]) 
-redirect URI.escape("/#{params["titulo"]}")
-end
 
 #Página universal, al final!!
 get "/:titulo" do
-  @titulo = params[:titulo]
-  @contenido = page_content(@titulo)
-  erb :mostrar
+# CODIGO SECRETO
+"HcUy6Re2LLBRtj"
 end
+
 
 
 # # LINK PODRÍA SER ÜTIL
